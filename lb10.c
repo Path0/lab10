b367f782715903a040834087e340049ccc6808b1
Patho <62456068+Path0@users.noreply.github.com>
2022-04-06 17:48:43 +0000

    1

--- lb10.c
+++ lb10.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int * generate();
+int getValue(int*, int);
 
 int main(void) {
+  int *xValues;
+  int *yValues;
+
+  xValues = generate();
+
+
+  int x;
+  for(x = 0; x < 10; x++)
+    {
+      printf("%d", getValue(xValues, x));
+    }
   
+  return 0;
+}
+
+int * generate()
+{
+  int xValues[500];
+  int index;
+
+  for(index = 0; index < 500; index++)
+  {
+      xValues[index] = (rand() % 201) - 100;
+  }
+
+  return xValues;
+}
+
+int getValue(int *array, int index)
+{
+  return *(array + index);
 }
